45b1b087698b1301c58a2d2519f5cde01208f318
josiaah <josi.programs@gmail.com>
2021-01-12 19:17:38 +0000

    bam games

---
+++ components/Navbar.vue
@@ -0,0 +1,78 @@
+<template>
+  <nav class="flex items-center justify-between flex-wrap p-6">
+    <div class="flex items-center flex-no-shrink mr-6">
+      <img src="~/assets/img/ga.png" class="w-20 h-20" />
+    </div>
+    <div class="block sm:hidden">
+      <button
+        @click="toggle"
+        class="flex items-center px-3 py-2 border rounded text-teal-lighter border-teal-light hover:text-white hover:border-white"
+      >
+        <svg
+          class="fill-current h-3 w-3"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <title>Menu</title>
+          <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+        </svg>
+      </button>
+    </div>
+    <div
+      :class="open ? 'block' : 'hidden'"
+      class="w-full flex-grow sm:flex sm:items-center sm:w-auto"
+    >
+      <div class="text-xl sm:flex-grow">
+        <a
+          href="#responsive-header"
+          class="no-underline montserrat font-medium text-gray-800 block mt-4 sm:inline-block sm:mt-0 mr-4"
+        >
+         Games & Groups
+        </a>
+        <a
+          href="#responsive-header"
+          class="no-underline montserrat font-medium text-gray-800 block mt-4 sm:inline-block sm:mt-0 mr-4"
+        >
+          Support & Information
+        </a>
+        <a
+          href="#responsive-header"
+          class="no-underline montserrat font-medium text-gray-800 block mt-4 sm:inline-block sm:mt-0 mr-4"
+        >
+          Partnerships
+        </a>
+        <a
+          href="#responsive-header"
+          class="no-underline montserrat font-medium text-gray-800 block mt-4 sm:inline-block sm:mt-0 mr-4"
+        >
+          Jobs
+        </a>
+      </div>
+      <div>
+        <a
+          href="#"
+          class="uppercase text-white no-underline inline-block font-bold text-md px-6 py-4 leading-none bg-green-500 hover:bg-green-600 rounded mt-4 sm:mt-0"
+          >Join the Group!</a
+        >
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      open: false,
+    };
+  },
+  methods: {
+    toggle: function () {
+      this.open = !this.open;
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <section class="py-12 px-4 text-center">
+    <div class="w-full max-w-2xl mx-auto">
+      <h2
+        class="capitalize text-gray-800 text-5xl mt-2 mb-2 leading-tight tracking-tight font-medium font-heading"
+      >
+        Explore new opportunities!
+      </h2>
+      <p class="mb-8 text-gray-700 leading-relaxed">
+        Here at BamGames the community is safe for everyone, no need to worry
+        about cyber bullying on our watch. We are reinventing the way people
+        play Roblox for future generations.
+      </p>
+      <div>
+        <a
+          class="uppercase inline-block sm:mb-4 py-4 px-8 mr-6 leading-none text-white bg-green-500 hover:bg-green-600 font-semibold rounded shadow"
+          href="#"
+          >Join the group!</a
+        ><a
+          class="uppercase inline-block sm:mb-4 py-4 px-8 mr-6 leading-none text-white bg-blue-500 hover:bg-blue-600 font-semibold rounded shadow"
+          href="#"
+        >
+          View our partners
+        </a>
+      </div>
+      <div class="mt-20">
+        <div class="max-w-lg mx-auto text-center">
+          <h3
+            class="text-4xl text-gray-700 tracking-tight leading-tight font-bold font-heading"
+          >
+            What we do at <span class="text-red-500">Bam Games</span>?
+          </h3>
+          <p class="mt-4 mb-6 text-gray-700 leading-loose">
+            We build and invent the impossible, you probably know us from some
+            of our simulator games!
+          </p>
+          <a
+            class="inline-block py-4 px-8 leading-none text-white bg-indigo-500 font-semibold rounded shadow"
+            href="#"
+            >Learn more</a
+          >
+        </div>
+      </div>
+      <div class="mt-10 flex flex-wrap -mx-4 -mb-4 md:mb-0">
+        <div class="w-full md:w-1/2 px-4 mb-4 md:mb-0">Stats</div>
+        <div class="w-full md:w-1/2 px-4 mb-4 md:mb-0">Place Visits</div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
